6f1412fe9fcb66e65eadcc6b7db715968c99e7b2
datnq1409 <Datnq2@bitnemo.vn>
2021-08-28 14:21:00 +0700

    update

--- src/main/java/com/example/demo/model/Word.java
+++ src/main/java/com/example/demo/model/Word.java
@@ -12,7 +12,7 @@ public class Word {
     private String pronounce;
     private String description;
 
-    @Column(length = 1000)
+    @Column(length = 2000)
     private String imageUrl;
 
     public Word() {
